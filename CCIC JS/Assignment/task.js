87ee2285aa549d9a4bc96fcdbaafd6aa06dc2cd8
Omar Abdullah <omarabdoelhantour@gmail.com>
2024-08-20 17:13:14 +0300

    Add files via upload

---
+++ CCIC JS/Assignment/task.js
@@ -0,0 +1,19 @@
+/*
+var x = 30;
+function hello(){
+    console.log(x);
+    x=10;
+    console.log(x);
+}
+hello();
+console.log(x);
+*/
+
+let y = 70;
+function hello() {
+    console.log(y);
+    y = 10;
+    console.log(y);
+}
+hello();
+console.log(y);
---
+++ CCIC JS/Assignment/task2.js
@@ -0,0 +1,34 @@
+let x =3;
+let y = 'omar';
+let user ={name:" omar " , age: 21 , date:" 15-7-2003 "};
+let student =[ 10 , 20 , 25 , 80 , 'kaled' ];
+student[4];
+const letter=new set([ "a" , "a" , "b" , "c" , "d" ]);           //مش بيعمل تكرار للي جواه 
+console.log(letter),
+
+typeof(x);
+typeof(user);
+typeof(student);
+
+x = x + user.name;
+x = x + user.age;
+console.log(user.date);
+
+let v = 10;
+console.log(v);
+function hellox(){
+    v = 20 ;
+    console.log(v);
+}
+hellox();
+console.log(v);
+
+
+var t =10;
+console.log(t);
+function hellot(){
+    let t = 20 ;
+    console.log(t);
+}
+hellot();
+console.log(t);
